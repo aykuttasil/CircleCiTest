c90d90ee79c33ba96f5ea10672466bbe1f0e5b57
Aykut Asil <aykuttasil@gmail.com>
2018-11-30 18:18:13 +0300

    update

---
+++ app/src/main/java/com/aykutasil/circlecitest/App.kt
@@ -0,0 +1,13 @@
+package com.aykutasil.circlecitest
+
+import android.app.Application
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Fabric.with(this, Crashlytics())
+    }
+}
--- app/src/main/java/com/aykutasil/circlecitest/MainActivity.kt
+++ app/src/main/java/com/aykutasil/circlecitest/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.aykutasil.circlecitest
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import com.crashlytics.android.Crashlytics
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnFabricTest).setOnClickListener {
+            Crashlytics.getInstance().crash()
+        }
     }
 }
